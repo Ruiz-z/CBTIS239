389eecfede11341743011f6989164d63a407373f
Ruiz-z <mauroruiz0404@gmail.com>
2025-09-24 07:07:49 -0600

    Se hacen cambios en la estructura del proyecto para mejor compresion

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,11 @@
 plugins {
-    id("java")
+
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
-
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
-
-tasks.test {
-    useJUnitPlatform()
+allprojects {
+    group = "com.cbtis239"
+    version = "0.1.0"
+    repositories {
+        mavenCentral()
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,2 @@
 rootProject.name = "CBTIS239"
-
+include("Backend","Frontend")
