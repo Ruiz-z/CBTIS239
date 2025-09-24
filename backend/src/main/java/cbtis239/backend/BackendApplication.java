fc33e951c2efa11bfa98a253e86cacf42f63d315
Ruiz-z <mauroruiz0404@gmail.com>
2025-09-24 08:24:08 -0600

    Se hacen cambios en la estructura del proyecto para mejor compresion

---
+++ backend/src/main/java/cbtis239/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package cbtis239.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/cbtis239/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cbtis239.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
