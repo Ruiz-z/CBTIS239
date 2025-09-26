4d201b76c8885477888e863b90591f3098d2a29f
Ruiz-z <mauroruiz0404@gmail.com>
2025-09-26 09:53:59 -0600

    feat(security): configuracion inicial para permitir endpints

---
+++ backend/src/main/java/com/cbtis239/escolares/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.cbtis239.escolares.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable());
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/**").permitAll() // TEMPORAL: luego lo cambiamos a JWT
+                .anyRequest().permitAll());
+        http.httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+}
