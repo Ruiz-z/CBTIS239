9e5812a364e91eafa8d556975e9343e4ec159e8c
Ruiz-z <mauroruiz0404@gmail.com>
2025-10-01 15:18:50 -0600

    chore(frontend): ajusta module-info para incluir dependencias JavaFX

--- front/src/main/java/module-info.java
+++ front/src/main/java/module-info.java
@@ -2,6 +2,9 @@ module cbtis239.front {
     requires javafx.controls;
     requires javafx.fxml;
 
+    requires org.kordamp.ikonli.core;
+    requires org.kordamp.ikonli.javafx;
+
     opens cbtis239.front to javafx.fxml;
     exports cbtis239.front;
 }
