4d11fcb9fef8bf045dba36e138363e7073a52583
Ruiz-z <mauroruiz0404@gmail.com>
2025-10-02 21:02:24 -0600

    refactor(frontend): actualiza MainApp para manejar navegación y registro

--- front/src/main/java/cbtis239/front/MainApp.java
+++ front/src/main/java/cbtis239/front/MainApp.java
@@ -1,5 +1,6 @@
 package cbtis239.front;
 
+import cbtis239.front.util.SceneNavigator;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -12,10 +13,8 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        // Ruta del FXML en resources (ajústala si tu archivo se llama distinto)
         final String fxmlPath = "/cbtis239/front/Login.fxml";
 
-        // Carga segura: si no existe, lanza una excepción con mensaje claro
         URL fxmlUrl = Objects.requireNonNull(
                 MainApp.class.getResource(fxmlPath),
                 "No se encontró el FXML en: " + fxmlPath + "\n" +
@@ -24,10 +23,9 @@ public class MainApp extends Application {
 
         FXMLLoader loader = new FXMLLoader(fxmlUrl);
 
-        // Crea la escena (tamaño similar al diseño que mostraste)
         Scene scene = new Scene(loader.load(), 740, 420);
 
-        // Si tu FXML NO incluye stylesheets, puedes añadir el CSS aquí:
+
          scene.getStylesheets().add(
              Objects.requireNonNull(
                  MainApp.class.getResource("/cbtis239/front/css/login.css"),
